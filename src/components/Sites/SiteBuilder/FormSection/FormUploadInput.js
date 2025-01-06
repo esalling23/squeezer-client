d3918368eec208ac10509c3ebaaacde8d76a3307
Eron Salling <esalling23@gmail.com>
2025-01-06 13:23:29 -0500

    Handles too-large file size for file upload field

--- src/components/Sites/SiteBuilder/FormSection/FormUploadInput.js
+++ src/components/Sites/SiteBuilder/FormSection/FormUploadInput.js
@@ -1,4 +1,4 @@
-import { Button, FileField, InputLabel } from "@mui/material"
+import { Box, Button, InputLabel, Typography } from "@mui/material"
 import React, { useState } from "react"
 import styled from "styled-components";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
@@ -15,28 +15,49 @@ const VisuallyHiddenInput = styled('input')({
   width: 1,
 });
 
-const FormUploadInput = ({ type = "image", name, label, setFormData, startingValue = '' }) => {
+const FormUploadInput = ({
+	type = "image",
+	name,
+	label,
+	handleFormChange,
+	startingValue = '',
+	helperText = ''
+}) => {
 	const [value, setValue] = useState(startingValue || '')
+	const [uploadError, setUploadError] = useState(null);
 
 	const handleChange = (e) => {
+		console.log(name, e.target.files[0])
+		// Approx 2mb
+		if(e.target.files[0].size > 2097152) {
+			setUploadError('File Too Large');
+			e.target.value = '';
+			return;
+		}
+		setUploadError(null);
 		setValue(e.target.files[0])
-		setFormData(curr => ({ ...curr, [name]: e.target.files[0] }))
+		handleFormChange({ [name]: e.target.files[0] })
 	}
 	return (
-		<Button
-			component="label"
-			role={undefined}
-			variant="contained"
-			tabIndex={-1}
-			startIcon={<CloudUploadIcon />}
-		>
+		<Box>
 			<InputLabel>{label}</InputLabel>
-			{value ? 'Change file' : 'Upload file'}
-			<VisuallyHiddenInput
-				type="file"
-				onChange={handleChange}
-			/>
-		</Button>
+
+			<Button
+				component="label"
+				role={undefined}
+				variant="contained"
+				tabIndex={-1}
+				startIcon={<CloudUploadIcon />}
+			>
+				{value ? 'Change file' : 'Upload file'}
+				<VisuallyHiddenInput
+					type="file"
+					onChange={handleChange}
+				/>
+			</Button>
+			{helperText && <Typography color='text.default'>{helperText}</Typography>}
+			{uploadError && <Typography color='secondary.main'>{uploadError}</Typography>}
+		</Box>
 	)
 }
 
