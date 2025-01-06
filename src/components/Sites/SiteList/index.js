6de7a50031e628c3c281e05d98a00397208cfb68
Eron Salling <esalling23@gmail.com>
2025-01-06 16:53:22 -0500

    Adds divider to SiteList

--- src/components/Sites/SiteList/index.js
+++ src/components/Sites/SiteList/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import Stack from '@mui/material/Stack'
 import RouterLink from '../../shared/RouterLink';
 import { useUserContext } from '../../../context/UserContext';
-import { Box, LinearProgress, ListItem } from '@mui/material';
+import { Box, LinearProgress, ListItem, Typography } from '@mui/material';
+import Divider from '@mui/material/Divider';
 
 import WebIcon from '@mui/icons-material/Web';
 
@@ -15,30 +16,37 @@ const SiteList =  () => {
 	}
 
 	const listItems = sites?.map(site => (
-		<ListItem key={site.id}>
-			<RouterLink to={`/sites/${site.id}`} sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        {site.heroImage ? (
-          <img src={site.heroImage} height="100" width="100" alt="hero" /> 
-        ) : <Box
-          sx={{
-            width: 100,
-            height: 100,
-            // backgroundColor: site.backgroundColor,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
-          <WebIcon fontSize="large"/>
-        </Box>}
-				{site.pageTitle}
-			</RouterLink>
-		</ListItem>
+    <>
+      <ListItem key={site.id}>
+        <RouterLink to={`/sites/${site.id}`} sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          {site.heroImage ? (
+            <img src={site.heroImage} height="100" width="100" alt="hero" /> 
+          ) : <Box
+            sx={{
+              width: 100,
+              height: 100,
+              // backgroundColor: site.backgroundColor,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+
+            }}
+          >
+            <WebIcon fontSize="large"/>
+          </Box>}
+          {site.pageTitle}
+        </RouterLink>
+      </ListItem>
+      <Divider variant='middle'/>
+    </>
 	))
 
 	return (
 		<Stack>
-			{listItems?.length > 0 ? listItems : 'No sites...yet!'}
+			{listItems?.length > 0 ? listItems : <Typography 
+        variant="h5" 
+        sx={{ textAlign: 'center', py: 3 }}
+      >No sites...yet!</Typography>}
 		</Stack>
 	);
 }
