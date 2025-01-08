07aea26e3c59bdfb4d62f75fa145b10064c5e600
Eron Salling <esalling23@gmail.com>
2025-01-07 19:48:12 -0500

    Adds site app bar breadcrumbs
    Closes https://github.com/esalling23/squeezer-client/issues/5

--- src/components/Sites/SiteAppBar/index.js
+++ src/components/Sites/SiteAppBar/index.js
@@ -1,12 +1,16 @@
 import React, { useMemo } from "react";
 import styled from "styled-components";
-import { useParams } from "react-router";
+import { useLocation, useParams } from "react-router";
 
-import { AppBar as mAppBar, Box, Container, Toolbar, Typography, Button } from "@mui/material"
+import { AppBar as mAppBar, Box, Container, Toolbar, Breadcrumbs } from "@mui/material"
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import HomeIcon from '@mui/icons-material/Home';
+import ContactsIcon from '@mui/icons-material/Contacts';
+import WebIcon from '@mui/icons-material/Web';
 
 import RouterLink from "../../shared/RouterLink";
 import CreateSiteButton from "../SiteActions/CreateSiteButton";
+import LinkButton from "../../shared/LinkButton";
 
 const AppBar = styled(mAppBar)(({ theme }) => ({
   boxShadow: 'none',
@@ -21,28 +25,69 @@ const AppBar = styled(mAppBar)(({ theme }) => ({
 const SiteAppBar = ({
 	site,
 }) => {
+
 	const { id } = useParams();
+  const location = useLocation();
+
 	const siteActions = useMemo(() => {
 		if (!id) {
 			return (
 				<CreateSiteButton />
 			)
 		}
-		return <RouterLink to={`/sites/${id}/leads`}>View Leads</RouterLink>
-	}, [id])
+    if (location.pathname.includes('/leads')) {
+      return <></>
+    }
+		return <LinkButton 
+      variant="contained"
+      to={`/sites/${id}/leads`}
+      size="large"
+    >Leads</LinkButton>
+	}, [id, location])
+
 	return (
 		<AppBar position="sticky">
-			<Container>
+			<Container maxWidth="xl">
 				<Toolbar disableGutters>
-					<Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'row' }}>
+          <Breadcrumbs 
+            sx={{ flexGrow: 1, display: 'flex', backgroundColor: 'inherit', boxShadow: 'none' }}
+            aria-label="breadcrumbs"
+          >
 						<RouterLink
 							to="/sites"
 							sx={{ opacity: id ? 1 : 0, pointerEvents: id ? 'auto' : 'none' }}
 						>
 							<ArrowBackIcon />
 						</RouterLink>
-						<Typography>Sites {site?.pageTitle ? ` / ${site.pageTitle}` : ''}</Typography>
-					</Box>
+            <RouterLink
+              underline="hover"
+              sx={{ display: 'flex', alignItems: 'center' }}
+              color="inherit"
+              to="/sites"
+            >
+              <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+              Sites
+            </RouterLink>
+            {site?.pageTitle && <RouterLink
+              underline="hover"
+              sx={{ display: 'flex', alignItems: 'center' }}
+              color="inherit"
+              to={`/sites/${site.id}`}
+            >
+              <WebIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+              {site.pageTitle}
+            </RouterLink>}
+
+            {location.pathname.includes('/leads') && <RouterLink
+              underline="hover"
+              sx={{ display: 'flex', alignItems: 'center' }}
+              color="inherit"
+              to={`/sites/${site.id}/leads`}
+            >
+              <ContactsIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+              Leads
+            </RouterLink>}
+          </Breadcrumbs>
 					<Box>
 						{/* Middle buttons? */}
 					</Box>
